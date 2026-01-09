56e2d64870f260ba2bca1e1e4673ee865335e66c
NirthyaThara <nirthyathara@gmail.com>
2026-01-09 22:29:14 +0530

    Fix broken images, remove redundant folders, and prepare for final deployment

--- client/src/features/products - Copy/product.routes.jsx
+++
@@ -1,9 +0,0 @@
-import ProductPage from "./pages/ProductPage";
-import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
-
-export default [
-  { path: "/prod", element: <ProductPage /> },
-  { path: "/prod/add", element: <AddProduct /> },
-  { path: "/prod/edit/:id", element: <EditProduct /> }
-];
