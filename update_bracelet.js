fc938c0daf7c791cdb99610336890ceefa9f1554
NirthyaThara <nirthyathara@gmail.com>
2026-01-10 00:37:53 +0530

    Update update_bracelet.js

--- update_bracelet.js
+++ update_bracelet.js
@@ -1,13 +1,1 @@
-require('dotenv').config({ path: './backend/.env' });
-const db = require('./backend/db');
-async function updateBracelet() {
-    try {
-        const [result] = await db.query("UPDATE Products SET category_id = 4 WHERE prod_name LIKE '%bracelet%'");
-        console.log(`Updated ${result.affectedRows} products.`);
-    } catch (err) {
-        console.error(err);
-    } finally {
-        process.exit();
-    }
-}
-updateBracelet();
+
